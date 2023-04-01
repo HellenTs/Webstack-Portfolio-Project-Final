c90718aa33c241854a8445bc42a97ab74ca82a8c
HellenTs <hellentsegaye2@gmail.com>
2023-04-01 09:19:58 +0300

    Front-end specialization final project - MRENA

---
+++ Script.js
@@ -0,0 +1,537 @@
+let products = {
+  data: [
+    {
+      productName: 'Zara T-shirt For Women',
+      category: 'T-shirt',
+      price: '30',
+      image: 'All/1.jpg',
+    },
+    {
+      productName: 'Zara T-shirt For Women',
+      category: 'T-shirt',
+      price: '49',
+      image: 'All/1a.jpg',
+    },
+    {
+      productName: 'Zara T-shirt For Women',
+      category: 'T-shirt',
+      price: '99',
+      image: 'All/1b.jpg',
+    },
+    {
+      productName: 'Zara T-shirt For Women',
+      category: 'T-shirt',
+      price: '29',
+      image: 'All/1c.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '116',
+      image: 'All/1f4a0eeed7233355c63a87814626a644.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '89',
+      image: 'All/2.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '189',
+      image: 'ALL/3.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/4.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/5.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'v',
+      price: '49',
+      image: 'All/5a.png',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/5b.png',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/5c.png',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/6.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/6a.png',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/6b.png',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/6c.png',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/7.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/8.jpg',
+    },
+    {
+      productName: 'Tarditional Habesha Kemis Dress',
+      category: 'Dress',
+      price: '49',
+      image: 'All/9.jpg',
+    },
+    {
+      productName: 'Zara Complete Dress For Women',
+      category: 'Dress',
+      price: '49',
+      image: 'All/10.jpg',
+    },
+    {
+      productName: 'Tarditional Habesha Kemis Dress',
+      category: 'Dress',
+      price: '49',
+      image: 'All/11.jpg',
+    },
+    {
+      productName: 'Tarditional Habesha KemisDress',
+      category: 'Dress',
+      price: '49',
+      image: 'All/12.jpg',
+    },
+    {
+      productName: 'Tarditional Habesha Kemis Dress',
+      category: 'Dress',
+      price: '49',
+      image: 'All/13.jpg',
+    },
+    {
+      productName: 'Zara Black Tag Intense Perfume',
+      category: 'Purfume',
+      price: '49',
+      image: 'All/14.jpg',
+    },
+    {
+      productName: 'Zara Coach Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/111.jpg',
+    },
+    {
+      productName: 'Zara Red Temptation Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/112.jpg',
+    },
+    {
+      productName: 'Zara Vibrant Leather Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/113.jpg',
+    },
+    {
+      productName: 'Yves Saint Laurent Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/114.jpg',
+    },
+    {
+      productName: 'Million Elixir Perfume By Paco rabanne',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/115.jpg',
+    },
+    {
+      productName: 'Zara Tobacco Colletion',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/116.jpg',
+    },
+    {
+      productName: 'Zara Night Pour Homme II',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/119.jpg',
+    },
+    {
+      productName: 'Cherry Smoothie',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/120.jpg',
+    },
+    {
+      productName: 'Zara Pocco Mini Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/121.jpg',
+    },
+    {
+      productName: 'Saffron & Orange Flowers Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/122.jpg',
+    },
+    {
+      productName: 'Zara Black Tag Intense Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/123.jpg',
+    },
+    {
+      productName: 'Zara Blue Edition For Him Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/124.jpg',
+    },
+    {
+      productName: 'Zara Red Edition For Him Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/125.jpg',
+    },
+    {
+      productName: 'Zara Black Tag Intense Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/126.jpg',
+    },
+    {
+      productName: 'Yves Saint Laurent Perfume',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/127.jpg',
+    },
+    {
+      productName: 'Blue Seduction Perfume For Him',
+      category: 'Perfume',
+      price: '49',
+      image: 'All/128.jpg',
+    },
+    {
+      productName: 'QARTRIM DUST RESISTANCE 210 Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/221.jpg',
+    },
+    {
+      productName: 'Tarditional Habesha Kemis Dress',
+      category: 'Dress',
+      price: '49',
+      image: 'All/13.jpg',
+    },
+    {
+      productName: 'Emporio Armani Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/222.jpg',
+    },
+    {
+      productName: 'JAXIGO MUD WALL Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/223.jpg',
+    },
+    {
+      productName: 'SWISH QUARTZ Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/224.jpg',
+    },
+    {
+      productName: 'SWISH QUARTZ Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/225.jpg',
+    },
+    {
+      productName: 'CK BLACK 125 Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/226.jpg',
+    },
+    {
+      productName: 'POLICE CHRONOGRAPH Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/227.jpg',
+    },
+    {
+      productName: 'HUGO BOSS Watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/228.jpg',
+    },
+    {
+      productName: 'Boss Hugo Boss watch',
+      category: 'Watch',
+      price: '49',
+      image: 'All/228.jpg',
+    },
+    {
+      productName: 'Polo Shoe For Men',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/330.jpg',
+    },
+    {
+      productName: 'Mk Shoe For Men',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/331.jpg',
+    },
+    {
+      productName: 'Zara Leather For Her Shoe',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/332.jpg',
+    },
+    {
+      productName: 'ZARA LEATHER For Him Shoe',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/333.jpg',
+    },
+    {
+      productName: 'NIKE SPORTS WEAR',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/334.jpg',
+    },
+    {
+      productName: 'NIKE AIR Shoe',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/335.jpg',
+    },
+    {
+      productName: 'JIMMY CHOO PURE LEATHER For Her Shoe',
+      category: 'She',
+      price: '49',
+      image: 'All/336.jpg',
+    },
+    {
+      productName: 'Zara Mini Leather For Her Shoe',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/337.jpg',
+    },
+    {
+      productName: 'Zara Mini Leather For Her Shoe',
+      category: 'Shoe',
+      price: '49',
+      image: 'All/338.jpg',
+    },
+    {
+      productName: 'MULTI FUNCTION MIXER AND BLENDER',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/444.jpg',
+    },
+    {
+      productName: 'BRAUN KG 7070 COFFEE GRINDER',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/550.jpeg',
+    },
+    {
+      productName: 'MULTI FUNCTION MIXER AND BLENDER',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/551.jpg',
+    },
+    {
+      productName: 'BRAUN FOOD PROCESSOR',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/552.jpeg',
+    },
+    {
+      productName: 'MULTI FUNCTION MIXER AND BLENDER',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/553.jpeg',
+    },
+    {
+      productName: 'BRAUN JUICE MIXER AND GRINDER',
+      category: 'Appliancs',
+      price: '49',
+      image: 'All/554.jpeg',
+    },
+    {
+      productName: 'Traditional Habasha Kemis Dress',
+      category: 'Dress',
+      price: '49',
+      image: 'All/555.jpeg',
+    },
+    {
+      productName: 'BRAUN MULTIQUICK 3MQ BLENDER',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/556.jpeg',
+    },
+    {
+      productName: 'BRAUN FOOD PROCESSOR',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/557.jpeg',
+    },
+    {
+      productName: 'BRAUN MULTI TOAST HT 550',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/558.jpeg',
+    },
+    {
+      productName: 'BRAUN BREAKFAST 1 Toaster',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/559.jpeg',
+    },
+    {
+      productName: 'Braun Series 9 Wet/Dry Electric Shaver',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/660.jpeg',
+    },
+    {
+      productName: 'BRAUN SERIES 5 Electric Shaver For Men',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/661.jpeg',
+    },
+    {
+      productName: 'Braun Electric Razor For Men ',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/662.jpeg',
+    },
+    {
+      productName: 'Sony Fm/Am Handy Portable Radio',
+      category: 'Appliances',
+      price: '49',
+      image: 'All/663.jpeg',
+    },
+  ],
+};
+
+for (let i of products.data) {
+  //Create Card
+  let card = document.createElement('div');
+  //Card should have category and should stay hidden initially
+  card.classList.add('card', i.category, 'hide');
+  //image div
+  let imgContainer = document.createElement('div');
+  imgContainer.classList.add('image-container');
+  //img tag
+  let image = document.createElement('img');
+  image.setAttribute('src', i.image);
+  imgContainer.appendChild(image);
+  card.appendChild(imgContainer);
+  //container
+  let container = document.createElement('div');
+  container.classList.add('container');
+  //product name
+  let name = document.createElement('h5');
+  name.classList.add('product-name');
+  name.innerText = i.productName.toUpperCase();
+  container.appendChild(name);
+  //price
+  let price = document.createElement('h6');
+  price.innerText = '$' + i.price;
+  container.appendChild(price);
+
+  card.appendChild(container);
+  document.getElementById('products').appendChild(card);
+}
+
+//parameter passed from button (Parameter same as category)
+function filterProduct(value) {
+  //Button class code
+  let buttons = document.querySelectorAll('.button-value');
+  buttons.forEach((button) => {
+    //check if value equals innerText
+    if (value.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add('active');
+    } else {
+      button.classList.remove('active');
+    }
+  });
+
+  //select all cards
+  let elements = document.querySelectorAll('.card');
+  //loop through all cards
+  elements.forEach((element) => {
+    //display all cards on 'all' button click
+    if (value == 'all') {
+      element.classList.remove('hide');
+    } else {
+      //Check if element contains category class
+      if (element.classList.contains(value)) {
+        //display element based on category
+        element.classList.remove('hide');
+      } else {
+        //hide other elements
+        element.classList.add('hide');
+      }
+    }
+  });
+}
+
+//Search button click
+document.getElementById('search').addEventListener('click', () => {
+  //initializations
+  let searchInput = document.getElementById('search-input').value;
+  let elements = document.querySelectorAll('.product-name');
+  let cards = document.querySelectorAll('.card');
+
+  //loop through all elements
+  elements.forEach((element, index) => {
+    //check if text includes the search value
+    if (element.innerText.includes(searchInput.toUpperCase())) {
+      //display matching card
+      cards[index].classList.remove('hide');
+    } else {
+      //hide others
+      cards[index].classList.add('hide');
+    }
+  });
+});
+
+//Initially display all products
+window.onload = () => {
+  filterProduct('all');
+};
+
